aa9f91a49573884df9fa269fbe133420295cedcd
Scott McPeak <scott.g.mcpeak@gmail.com>
2021-04-28 18:31:04 -0700

    mkskel.sh: Change to accept name of contents file and array name.
    I am doing this in preparation to similarly embed FlexLexer.h in
    the flex executable, so I need mkskel.sh to be generalized.
    
    Makefile.in: Adjust invocation of mkskel.sh accordingly.
    
    skel.c: Regenerate, thereby adding a comment.

--- mkskel.sh
+++ mkskel.sh
@@ -1,16 +1,44 @@
 #! /bin/sh
+# Create a C source file containing a string literal that contains
+# the contents of the given file.
 
-cat <<!
-/* File created from flex.skl via mkskel.sh */
+if [ "x$3" = "x" ]; then
+  echo "usage: $0 <contents file> <dest file> <array name>"
+  echo ""
+  echo "Creates <dest file>, a C source code file, which defines an "
+  echo "array called <array name> whose contents is <contents file>."
+  exit 2
+fi
+
+contents_file="$1"
+dest_file="$2"
+array_name="$3"
+
+# File descriptor 3 is our output.
+exec 3>"$dest_file"
+
+cat >&3 <<!
+/* File created from $contents_file via $0 */
 
 #include "flexdef.h"
 
-const char *skel[] = {
+/* NULL-terminated array of NUL-terminated lines, *without* line
+ * terminators. */
+const char *$array_name[] = {
 !
 
-sed 's/\\/&&/g' $* | sed 's/"/\\"/g' | sed 's/.*/  "&",/'
+# Step 1: Escape backslashes.
+# Step 2: Escape double-quotes.
+# Step 3: Surround every line with double-quotes.
+sed 's/\\/&&/g' "$contents_file" | sed 's/"/\\"/g' | sed 's/.*/  "&",/' >&3
 
-cat <<!
+# A single NULL pointer terminates the array.
+cat >&3 <<!
   0
 };
 !
+
+# Close fd 3.
+exec 3>&-
+
+# EOF
